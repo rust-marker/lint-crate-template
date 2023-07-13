69f8d4754ce774f6bb130ada955e9a62adb60d28
xFrednet <xFrednet@gmail.com>
2023-07-13 12:48:38 +0200

    Minimal `LintPass` setup :pen:

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,29 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use marker_api::{LintPass, LintPassInfo, LintPassInfoBuilder};
+
+#[derive(Default)]
+struct MyLintPass {}
+marker_api::export_lint_pass!(MyLintPass);
+
+marker_api::declare_lint! {
+    /// # What it does
+    /// Here you can explain what your lint does. The description supports normal
+    /// markdown.
+    ///
+    /// # Example
+    /// ```rs
+    /// // Bad example
+    /// ```
+    ///
+    /// Use instead:
+    /// ```rs
+    /// // Good example
+    /// ```
+    MY_LINT,
+    Warn,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl LintPass for MyLintPass {
+    fn info(&self) -> LintPassInfo {
+        LintPassInfoBuilder::new(Box::new([MY_LINT])).build()
     }
 }
